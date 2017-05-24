0fbdfb87e0f73f05c13dac27c40c4017ff7d5529
jxmot <webdev@stupiddog.net>
2017-05-24 02:20:44 -0500

    added use of SITE_ID, removed unused define() calls

--- site.php
+++ site.php
@@ -1,4 +1,7 @@
 <?php
+$_site_id = 'demo_01';
+define('SITE_ID', $_site_id);
+
 /*
     Register API keys at - 
         https://www.google.com/recaptcha/admin
@@ -17,8 +20,6 @@ define('SITE_CHARSET', 'UTF-8');
 // the page that is loaded upon passing the reCAPTCHA and
 // entering a user name
 define('SITE_APPL', './phpinfo.php');
-// used by callerid.php
-define('SITE_USERIP', $_SERVER['REMOTE_ADDR']);
 /*
     some page elements
 */
@@ -26,11 +27,4 @@ define('PAGE_TITLE', 'Title for this Page goes here');
 define('PAGE_HEADING', 'Page Heading');
 // Submit button caption
 define('FORM_SUBMIT', 'Enter');
-/*
-    For use in subsequent code, for example the 
-    counter might use this to create page named
-    counters. 
-*/
-$tmp = basename($_SERVER['PHP_SELF']);
-define('SITE_RESOURCE', substr($tmp, 0, (strlen($tmp) - 4)));
 ?>
