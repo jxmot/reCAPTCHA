fe96c2513190ee8f050b58eda5943494c062582f
jxmot <webdev@stupiddog.net>
2017-05-24 02:22:28 -0500

    added HTML to output, added use of session site_id

--- phpinfo.php
+++ phpinfo.php
@@ -1,4 +1,6 @@
 <?php 
+session_start();
+
 /*
     Reads the userName from the session that
     was written to by a prior validation event.
@@ -6,7 +8,8 @@
     this file was paired with a reCAPTCHA v2
     validation.
 */
-echo "userName from the reCAPTCHA page is - ".$_SESSION['userName']."\n\n";
+echo "<h2>userName from the reCAPTCHA page is - ".$_SESSION['userName']."</h2><br>";
+echo "<h2>site_id from the reCAPTCHA page is - ".$_SESSION['site_id']."</h2><br>";
 
 phpinfo(); 
 
