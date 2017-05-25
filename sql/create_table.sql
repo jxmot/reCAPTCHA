a7ec3e560d589584b11b6900ba53a91a034e5023
jxmot <webdev@stupiddog.net>
2017-05-24 21:36:48 -0500

    comments and added reCAPTCHA theme setting

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -9,7 +9,8 @@ create table sites(
     -- "site_id" - a string that uniquely identifies this data row. A 
     -- possible choice could be the associated domain name (without the TLD) 
     -- or application name. Keep it simple. This is also used by the optional 
-    -- caller ID and hit count code when naming their output files.
+    -- caller ID and hit count code when naming their output files. However, 
+    -- this column cannot be null or empty. It must contain a unique string.
     site_id varchar(32)  not null,
     -- Register API keys at - 
     --     https://www.google.com/recaptcha/admin
@@ -27,7 +28,9 @@ create table sites(
     page_heading varchar(32) default 'Page Heading',
     page_message varchar(255) default 'A message of up to 255 characters can be here.',
     -- submit button caption
-    form_submit varchar(16) default 'Enter'
+    form_submit varchar(16) default 'Enter',
+    -- reCAPTCHA theme (light or dark)
+    recaptcha_theme varchar(8) default 'dark'
 );
 
 select * from sites;
