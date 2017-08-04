411f9bc5af9bc48c468d04f4cefb67aa0c5cb3ee
jxmot <webdev@stupiddog.net>
2017-08-04 16:46:55 -0500

    FIX: call to SQL insert was broken

--- sql/seed.sql
+++ sql/seed.sql
@@ -12,10 +12,9 @@ use recaptcha_dev;
 -- write to all columns
 insert into sites (site_id,site_key,site_secret,site_lang,site_charset,site_appl,page_title,page_heading,page_message,form_submit,recaptcha_theme) 
 values ('demo_01','YOUR RECAPTCHA SITE KEY GOES HERE','YOUR RECAPTCHA SECRET GOES HERE','en','UTF-8','./phpinfo.php','reCAPTCHA Demo','reCAPTCHA Demo using MySQL','This demonstration illustrates reCAPTCHA v2 and MySQL, where MySQL is used for storing things like site specific content. Like this message.','Run Application','dark');
-values ('demo_02','YOUR RECAPTCHA SITE KEY GOES HERE','YOUR RECAPTCHA SECRET GOES HERE','en','UTF-8','./phpinfo.php','reCAPTCHA Demo','reCAPTCHA Demo using MySQL','This demonstration illustrates reCAPTCHA v2 and MySQL, where MySQL is used for storing things like site specific content. Like this message.','Run Application','light');
 
 -- minimum required columns for creating a row
--- insert into sites (site_id, site_key,site_secret) 
--- values ('demo_01','YOUR RECAPTCHA SITE KEY GOES HERE','YOUR RECAPTCHA SECRET GOES HERE');
+insert into sites (site_id,site_key,site_secret) 
+values ('demo_02','YOUR RECAPTCHA SITE KEY GOES HERE','YOUR RECAPTCHA SECRET GOES HERE');
 
 select * from sites;
