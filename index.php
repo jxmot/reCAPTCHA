5f5ab2781bbd31eb1617b457257c3f099a4dafe8
jxmot <webdev@stupiddog.net>
2020-09-25 19:24:52 -0500

    overhauled to work with recaptcha.php to v1.2.4 classes

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <?php
 session_start();
 
+// control over the use of MySQL
+define('USE_MYSQL', true);
+// if false or undefined then recaptchalib.php will be used
+// if true then the slightly modified v1.2.4 will be used
+define('USE_V124', true);
+
 // site and page specific variables...
-// MySQL version - 
-require_once "sitedb.php";
-// -- OR --
-// No db version
-//require_once "site.php";
+if(defined('USE_MYSQL') && USE_MYSQL === true) {
+        // MySQL version - 
+        require_once "_sitedb.php";
+    } else { // -- OR --
+        // No db version
+        require_once "site.php";
+}
 
 /* ********************************************************
     reCAPTCHA 
@@ -14,7 +22,11 @@ require_once "sitedb.php";
     Google's reCAPTCHA method for validating a vistor vs.
     a bot. 
 */
-require_once "recaptchalib.php";
+if(!defined('USE_V124') || USE_V124 === false) {
+    require_once "recaptchalib.php";
+} else {
+    require_once "./ReCaptcha/ReCaptcha.php";
+}
 
 // The response from reCAPTCHA
 $resp = null;
@@ -22,28 +34,38 @@ $resp = null;
 $error = null;
 
 // get a ReCaptcha object....
-$reCaptcha = new ReCaptcha(SITE_SECRET);
+if(!defined('USE_V124') || USE_V124 === false) {
+    $reCaptcha = new ReCaptcha(SITE_SECRET);
+} else {
+    $reCaptcha = new \ReCaptcha\ReCaptcha(SITE_SECRET);
+}
 
 // Was there a reCAPTCHA response?
 if ($_POST["g-recaptcha-response"]) {
-    $resp = $reCaptcha->verifyResponse(
-        $_SERVER["REMOTE_ADDR"],
-        $_POST["g-recaptcha-response"]
-    );
+    if(!defined('USE_V124') || USE_V124 === false) {
+        $resp = $reCaptcha->verifyResponse(
+            $_SERVER["REMOTE_ADDR"],
+            $_POST["g-recaptcha-response"]
+        );
+    } else {
+        $resp = $reCaptcha->verify(
+            $_POST["g-recaptcha-response"],
+            $_SERVER["REMOTE_ADDR"]
+        );
+    }
 }
 /* ********************************************************
     Optional silent hit counter and a visitor log of date/
     time and ip address.
 */
-//require_once "count.php";
-//require_once "callerid.php";
+require_once "count.php";
+require_once "callerid.php";
 ?>
 <!DOCTYPE html>
 <html lang="<?php echo SITE_LANG;?>">
 <meta charset="<?php echo SITE_CHARSET;?>">
 <head>
     <title><?php echo PAGE_TITLE;?></title>
-    <script src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
     <style>
         body {
             background:#333333;
@@ -72,8 +94,18 @@ if ($_POST["g-recaptcha-response"]) {
 </head>
 <body>
 <?php
+$success = false;
+
 // Did we arrive here as the result of the POST?
-if ($resp != null && $resp->success) {
+if($resp != null) {
+    if(!defined('USE_V124') || USE_V124 === false) {
+        $success = $resp->success;
+    } else {
+        $success = $resp->isSuccess();
+    }
+}
+
+if ($success == true) {
     // Save the userName in the session before
     // saving and closing the session.
     $_SESSION['userName']=$_POST['userName'];
@@ -94,6 +126,14 @@ if ($resp != null && $resp->success) {
     <div class="text-center">
         <h1><?php echo PAGE_HEADING;?></h1>
         <h3><?php echo PAGE_MESSAGE;?></h3>
+<?php
+if(!defined('USE_V124') || USE_V124 === false) {
+    echo "        <h3>Using <pre>recaptchalib.php - ";
+} else {
+    echo "        <h3>Using <pre>reCAPTCHA PHP client library - ";
+}
+echo $reCaptcha->getVersion() ."</pre></h3>\n";
+?>
         <form action="" method="post">
             <br/>
             <br/>
